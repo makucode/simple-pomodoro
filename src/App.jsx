024407ffe4a86042af58413ce121160b4ca5a534
maku <makucode@gmail.com>
2022-01-23 18:25:51 +0100

    added page load animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { AnimatePresence, motion } from "framer-motion";
 import Timer from "./components/Timer";
 import { TimerProvider } from "./contexts/TimerContext";
+import { fadeInUp } from "./utils/animations";
 
 const App = () => {
+    const animations = fadeInUp;
+
     return (
         <div className="App">
-            <TimerProvider>
-                <Timer />
-            </TimerProvider>
+            <AnimatePresence exitBeforeEnter>
+                <motion.div
+                    transition={{ duration: 0.5 }}
+                    initial="pageInitial"
+                    animate="pageAnimate"
+                    exit="pageExit"
+                    variants={animations}
+                >
+                    <TimerProvider>
+                        <Timer />
+                    </TimerProvider>
+                </motion.div>
+            </AnimatePresence>
         </div>
     );
 };
